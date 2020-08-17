0bd72cfa69781b2a45c720f55bbd488b08950d1e
Julian Silvestri <juliansilvestri92@gmail.com>
2020-08-17 12:00:25 -0400

    test

--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print("test")
+        print("this is a test")
     }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
