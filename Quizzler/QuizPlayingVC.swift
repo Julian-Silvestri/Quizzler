71912c7fc944b996b344275ac17b249a61eaf4e7
Julian Silvestri <juliansilvestri92@gmail.com>
2020-09-09 18:30:06 -0400

    saving work

--- Quizzler/QuizPlayingVC.swift
+++ Quizzler/QuizPlayingVC.swift
@@ -11,21 +11,35 @@ import AVFoundation
 
 class QuizPlayingVC: UIViewController {
 
+    @IBOutlet weak var statusViewContainer: UIView!
+    @IBOutlet weak var status1: UIView!
+    @IBOutlet weak var status2: UIView!
+    @IBOutlet weak var status3: UIView!
+    @IBOutlet weak var status4: UIView!
+    @IBOutlet weak var status5: UIView!
+    @IBOutlet weak var question1: UILabel!
+    @IBOutlet weak var quitbtn: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.status1.layer.cornerRadius = 7
+        self.statusViewContainer.layer.cornerRadius = 7
+        self.quitbtn.layer.cornerRadius = 5
 
     }
 
 
-//    @IBAction func test(_ sender: Any) {
-//        let string = "Hello, World!"
-//        let utterance = AVSpeechUtterance(string: string)
-//        utterance.voice = AVSpeechSynthesisVoice(language: "en-US")
-//
-//        let synth = AVSpeechSynthesizer()
-//        synth.speak(utterance)
-//
-//    }
+    @IBAction func quitBtn(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    @IBAction func test(_ sender: Any) {
+        
+        let utterance = AVSpeechUtterance(string: self.question1.text!)
+        utterance.voice = AVSpeechSynthesisVoice(language: "en-US")
+
+        let synth = AVSpeechSynthesizer()
+        synth.speak(utterance)
+
+    }
     
 }
